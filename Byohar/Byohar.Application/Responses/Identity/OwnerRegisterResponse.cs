697ba2b3007b120a4589841f615eb0da82882a91
Dheeraj Soni <dheeraj.viram@gmail.com>
2026-05-13 02:06:05 +0530

    Authentication full and final

---
+++ Byohar/Byohar.Application/Responses/Identity/OwnerRegisterResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Byohar.Application.Responses.Identity;
+
+public class OwnerRegisterResponse
+{
+    public Guid TenantId { get; set; }
+    public Guid UserId { get; set; }
+    public string TenantName { get; set; }
+    public string Email { get; set; }
+    public string Role { get; set; }
+}
